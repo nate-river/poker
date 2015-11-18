03f066910510cda0c4156d4dbbc96dbe59621f16
Ma Yanlong <mayanlong7@icloud.com>
2015-11-18 17:16:20 +0800

    re write

--- scripts/index.js
+++ scripts/index.js
@@ -1,163 +1,106 @@
 window.onload = function(){
-  var el, data = {length:0},dict = [],
-      sence = document.getElementById('sence'),
-      huase = ['ac','ad','ah','as'],
-      guize = {1:'A',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',10:'10',11:'J',12:'Q',13:'K'},
-      change = document.getElementById('change'),
-      changePlage = document.getElementById('change-place'),
-      reChange = document.getElementById('re-change'),
-      leftPlace = document.getElementById('left-place'),
-      removedEl;
-
-  change.onclick = function(){
-    if( !leftPlace.children.length) {return;}
-    removedEl = leftPlace.removeChild(leftPlace.lastElementChild);
-    removedEl.style.top = 0;
-    removedEl.style.left = 0;
-    removedEl.style.position = 'absolute';
-    changePlage.appendChild(removedEl);
+  var $ = function(id){
+    return document.getElementById(id);
   };
-  var dianjicishu = 0;
-  reChange.onclick = function(){
-    if(dianjicishu == 3 || leftPlace.children.length != 0) return;
-    while(changePlage.children.length){
-      removedEl = changePlage.removeChild(changePlage.lastElementChild);
-      leftPlace.appendChild(removedEl);
-    }
-    dianjicishu++;
-  };
+  var sence = $('sence'),
+      moveRight  = $('move-to-right'),
+      moveLeft  = $('move-to-left'),
+      leftSet = $('left-set'),
+      rightSet = $('right-set'),
+      previous;
 
-  while( data.length !== 52){
-    var t1  = huase[Math.floor(Math.random()*4)];
-    var t2  = 1 + Math.floor(Math.random()*13);
-    var id = t1 + ':' + t2;
-    if( !data[id] ){
-      data[id] = true;
-      data.length += 1; dict.push(id);
-    }
-  }
-  var dingwei = [];
-  for ( var i = 0;  i < 7;  i++){
-    for ( var j = 0;  j < i+1 ;  j++){
-      dingwei.push({top:i*75,left:(6-i)*(190/2)+j*210,zIndex:i, id:i+'_'+j });
-    }
-  }
-  for (i = 0;  i < 24;  i++){
-    dingwei.push({top:0,left:0,zIndex:0});
-  }
-
-  for ( i = 0;  i < 52;  i++){
-    el = document.createElement('div');
-    el.setAttribute('class','puke');
-    var str = dict[i];
-    el.innerHTML = guize[str.split(':')[1]];
-    el.setAttribute('data',str.split(':')[1]);
-    el.style.background = 'url(./images/western_'+str.split(':')[0]+'.png)';
-    el.style.top = dingwei[i].top + 'px';
-    el.style.left = dingwei[i].left + 'px';
-    el.style.zIndex = dingwei[i].zIndex;
-    if( str.split(':')[0] == 'ad' || str.split(':')[0] == 'ah'){
-      el.style.color = 'rgb(133,21,20)';
-    }
-    if(i<=27){
-      sence.appendChild(el);
-      el.setAttribute('id',dingwei[i].id);
-    }else{
-      leftPlace.appendChild(el);
-    }
-  }
-  var compare = [false,false];
-  var xiaochu  = function(){
-    if(compare[0] && compare[1] && compare[0].n + compare[1].n == 13){
-      compare[0].d.parentElement.removeChild(compare[0].d);
-      compare[1].d.parentElement.removeChild(compare[1].d);
-      compare = [false,false];
-      return true;
-    }
-    return false;
-  };
-
-  var tmp;
-  sence.onclick = function(e){
-    el = e.target;
-    if(el == leftPlace || el == this || el == change ||el == changePlage || el == reChange){return;}
-    var dd = Number(el.getAttribute('data'));
-    if( el.hasAttribute('id') ){
-      var x = Number(el.id.split('_')[0]);
-      var y = Number(el.id.split('_')[1]);
-      var x1 = (x+1) + '_' + y;
-      var x2 = (x+1) + '_' + (y+1);
-      var z1 = document.getElementById(x1);
-      var z2 = document.getElementById(x2);
-      console.log(z1,z2);
-      if(z1 || z2){
-        return;
+  var generatePoker = function(){
+    var colors = ['ac','ad','ah','as'],
+        dict  = {}, poker = [];
+    while( poker.length !== 52 ){
+      var  color  =  colors[ Math.floor(Math.random()*4) ];
+      var  number =  Math.floor( Math.random()*13 + 1 );
+      var key = color + number;
+      if( !dict[key] ){
+        poker.push( {color:color,number:number} );
+        dict[key] = true;
       }
     }
-    if(dd == 13){
-      el.parentElement.removeChild(el);return;
+    return poker;
+  };
+  var isFree = function(el){
+    var x = Number( el.id.split('_')[0] ),
+        y = Number( el.id.split('_')[1] ),
+        childLeftId = (x + 1) + '_' + y,
+        childRightId = (x + 1) + '_' + ( y + 1 );
+    if( $(childLeftId) || $(childRightId) ) return false;
+    return true;
+  };
+  sence.onclick = function(e){
+    var el = e.target;
+    if (el == this || el==moveLeft
+        || el == moveRight || el.hasAttribute('id') && !isFree(el) ){
+      if(previous){
+        previous.style.border = 'none';
+        previous = null;
+      }
+      return;
     }
-    if(!compare[0] && !compare[1]){
-      compare[0] = {};
-      compare[0].n = dd;
-      compare[0].d = el;
-      el.style.boxShadow = '0 0 10px black';
-      tmp  = el;
-    }else{
-      compare[1] = {};
-      compare[1].n = dd;
-      compare[1].d = el;
-      if( !xiaochu() ){
-        compare[0].d.style.boxShadow = 'none';
-        compare[1].d.style.boxShadow = 'none';
-        compare = [false,false];
-      };
+    if( el.getAttribute('data') == '13'){
+      el.parentElement.removeChild(el);
+      return;
     }
-
+    if( previous &&
+        Number(previous.getAttribute('data')) +
+        Number(el.getAttribute('data')) == 13 ){
+          previous.parentElement.removeChild(previous);
+          el.parentElement.removeChild(el);
+          return;
+        }
+    if(previous){
+      previous.style.border = 'none';
+    }
+    el.style.border = '2px solid #039219';
+    previous = el;
   };
 
-  // var TreeNode = function(val){
-  //   this.val = val;
-  //   this.left = this.right = null;
-  //   this.children = 0;
-  // };
-  // var BST = function(){
-  //   this.root = null;
-  // };
-  // BST.prototype.insert = function(data){
-  //   var n = new TreeNode(data);
-  //   if(this.root == null){
-  //     this.root = n;
-  //   }
-  //   else{
-  //     var parent = this.root;
-  //     while(true){
-  //       if(parent.left == null){
-  //         parent.left = n;
-  //         parent.children += 1;
-  //         break;
-  //       }else if(parent.right == null){
-  //         parent.right = n;
-  //         parent.children += 1;
-  //         break;
-  //       }else{
-  //         if(parent.left.children !==2){
-  //           parent = parent.left;
-  //         }else{
-  //           parent = parent.right;
-  //         }
-  //       }
-  //     }
-  //   }
-  // };
-  // var t = new BST();
-  // t.insert(20);
-  // t.insert(12);
-  // t.insert(14);
-  // t.insert(18);
-  // t.insert(12);
-  // t.insert(18);
-  // t.insert(12);
-  // console.log(t);
-
+  moveRight.onclick  = function(){
+    if( !leftSet.children.length) {return;}
+    rightSet.appendChild(leftSet.lastElementChild);
+  };
+  moveLeft.onclick = (function(){
+    var count = 0;
+    return function(){
+      if( count == 3 || leftSet.children.length != 0) return;
+      while(rightSet.children.length){
+        leftSet.appendChild(rightSet.lastElementChild);
+      }
+      count++;
+    };
+  })();
+  sence.onmousedown = function(e){e.preventDefault();};
+  (function(){
+    var index = 0, poker = generatePoker(), el,
+        guize = {1:'A',11:'J',12:'Q',13:'K'};
+    for ( var i = 0;  i < 7;  i++){
+      for ( var j = 0;  j < i+1;  j++){
+        el = document.createElement('div');
+        el.setAttribute('class','poker');
+        el.setAttribute('id',i + '_' + j );
+        el.setAttribute('data',poker[index].number);
+        el.style.top = 50*i + 'px';
+        el.style.left = (6-i)*60 + j*120 + 'px';
+        el.innerHTML = guize[ poker[index].number ]?
+          guize[poker[index].number]:poker[index].number;
+        el.style.backgroundImage = 'url(./images/western_'+
+          poker[index++].color + '.png)';
+        sence.appendChild(el);
+      }
+    }
+    for ( i = 0;  i < 24;  i++){
+      el = document.createElement('div');
+      el.setAttribute('class','poker remain-poker');
+      el.setAttribute('data',poker[index].number);
+      el.innerHTML = guize[ poker[index].number ]?
+        guize[poker[index].number]:poker[index].number;
+      el.style.backgroundImage = 'url(./images/western_'+
+        poker[index++].color + '.png)';
+      leftSet.appendChild(el);
+    }
+  })();
 };
